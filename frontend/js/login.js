382c531d7838c7c19cc9fd4f62f981124f29f773
Wrong Password <kamalrokaya185@gmail.com>
2026-07-17 15:35:10 +0545

    Stop tracking frontend folder

--- frontend/js/login.js
+++
@@ -1,8 +0,0 @@
-const form = document.querySelector('form');
-
-if (form) {
-    form.addEventListener('submit', (event) => {
-        event.preventDefault();
-        window.location.href = 'patient-dashboard.html';
-    });
-}
